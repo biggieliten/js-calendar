37bafd59c05135174b4f5ac93cd05502abe7727d
Jonas Persson <jonas.persson111@gmail.com>
2026-05-03 16:29:03 +0200

    render calendar properly with dates

--- script/calendar.js
+++ script/calendar.js
@@ -4,6 +4,7 @@ const calendarEl = document.getElementById("calendar-container");
 const monthYearInfoEl = document.getElementById("calendar-month-year");
 const decreaseMonthButton = document.getElementById("decrease-month");
 const increaseMonthButton = document.getElementById("increase-month");
+const calendarDesiredCardCount = 42;
 
 export function InitializeCalendar() {
   renderCalendar();
@@ -16,8 +17,66 @@ function renderCalendar() {
   setMonthYearInfo();
 
   calendarEl.innerHTML = null;
-  for (let i = 0; i < 42; i++) {
-    calendarEl.append(createCalendarCard(i));
+  renderPreviousMonth();
+  renderCurrentMonth();
+  renderNextMonth();
+}
+
+function renderPreviousMonth() {
+  let firstdayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+    1,
+  );
+
+  let daysNeeded = getDayCountForPreviousMonth(firstdayOfMonth.getDay());
+
+  const previousMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+    0 - daysNeeded,
+  );
+
+  for (let i = daysNeeded; i > 0; i--) {
+    let dayDate = new Date(
+      previousMonth.getFullYear(),
+      previousMonth.getMonth(),
+      previousMonth.getDay() - i,
+    );
+
+    let card = createCalendarCard(dayDate);
+    calendarEl.append(card);
+  }
+}
+
+function renderCurrentMonth() {
+  let monthStart = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+    1,
+  );
+
+  let currentMonth = monthStart.getMonth();
+
+  while (currentMonth === monthStart.getMonth()) {
+    calendarEl.append(createCalendarCard(monthStart));
+
+    monthStart.setDate(monthStart.getDate() + 1);
+  }
+}
+
+function renderNextMonth() {
+  let nextMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset + 1,
+    1,
+  );
+
+  let currentCardCount = calendarEl.childElementCount;
+
+  for (let i = currentCardCount; i < calendarDesiredCardCount; i++) {
+    calendarEl.append(createCalendarCard(nextMonth));
+    nextMonth.setDate(nextMonth.getDate() + 1);
   }
 }
 
@@ -27,7 +86,7 @@ function createCalendarCard(date) {
 
   let cardInfo = document.createElement("p");
   cardInfo.classList.add("calendar-card-info");
-  cardInfo.textContent = date;
+  cardInfo.textContent = date.getDate();
 
   newCard.append(cardInfo);
 
@@ -51,4 +110,25 @@ function changeMonth(change) {
   monthOffset += change;
 
   renderCalendar();
+}
+
+function getDayCountForPreviousMonth(dayOfWeek) {
+  switch (dayOfWeek) {
+    case 1:
+      return 0;
+    case 2:
+      return 1;
+    case 3:
+      return 2;
+    case 4:
+      return 3;
+    case 5:
+      return 4;
+    case 6:
+      return 5;
+    case 0:
+      return 6;
+    default:
+      return -1;
+  }
 }
