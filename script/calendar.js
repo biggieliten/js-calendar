2c4d986831eabe3566ef08f5345b9045489038eb
Jonas Persson <jonas.persson111@gmail.com>
2026-05-03 15:12:46 +0200

    renders month and year

--- script/calendar.js
+++ script/calendar.js
@@ -1,11 +1,16 @@
 const currentDate = new Date();
+let monthOffset = 0;
 const calendarEl = document.getElementById("calendar-container");
+const monthYearInfoEl = document.getElementById("calendar-month-year");
 
 export function InitializeCalendar() {
   renderCalendar();
 }
 
 function renderCalendar() {
+  setMonthYearInfo();
+
+  calendarEl.innerHTML = null;
   for (let i = 0; i < 42; i++) {
     calendarEl.append(createCalendarCard(i));
   }
@@ -22,4 +27,17 @@ function createCalendarCard(date) {
   newCard.append(cardInfo);
 
   return newCard;
+}
+
+function setMonthYearInfo() {
+  monthYearInfoEl.textContent = null;
+  let date = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() - monthOffset,
+  );
+
+  monthYearInfoEl.textContent = date.toLocaleDateString("sv", {
+    month: "long",
+    year: "numeric",
+  });
 }
