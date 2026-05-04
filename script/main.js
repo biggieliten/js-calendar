da8ffa70f5ee5dc3bf699e1562eed2218e5c9be2
Jonas Persson <jonas.persson111@gmail.com>
2026-05-04 11:23:35 +0200

    Made calendar weekday row show shortened names instead of resizing font

--- script/main.js
+++ script/main.js
@@ -2,11 +2,13 @@ import { switchView } from "./navbar.js";
 import { loadTodoCards } from "./todo.js";
 import { initializeWelcomeMessage } from "./welcomeMessage.js";
 import { InitializeCalendar } from "./calendar.js";
+import { initializeCalendarDays } from "./calendarDays.js";
 
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
   InitializeCalendar();
+  initializeCalendarDays();
   loadTodoCards();
   initializeWelcomeMessage();
   switchView();
