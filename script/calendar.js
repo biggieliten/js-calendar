726c671d917434d29fe72c9cac8d81b2366523b0
therealdanmalmx <danmalmx@gmail.com>
2026-05-05 14:21:17 +0200

    Add alternative text or dot for holiday

--- script/calendar.js
+++ script/calendar.js
@@ -8,245 +8,252 @@ const calendarDesiredCardCount = 42;
 let todos;
 
 const dateOptions = {
-	weekday: "short",
-	day: "2-digit",
-	year: "2-digit"
+  weekday: "short",
+  day: "2-digit",
+  year: "2-digit",
 };
 
 export async function initializeCalendar(incomingTodos) {
-	todos = incomingTodos;
+  todos = incomingTodos;
 
-	await initHolidays();
-	renderCalendar();
+  await initHolidays();
+  renderCalendar();
 
-	decreaseMonthButton.addEventListener("click", () => changeMonth(-1));
-	increaseMonthButton.addEventListener("click", () => changeMonth(1));
+  decreaseMonthButton.addEventListener("click", () => changeMonth(-1));
+  increaseMonthButton.addEventListener("click", () => changeMonth(1));
 }
 
 function renderCalendar() {
-	setMonthYearInfo();
+  setMonthYearInfo();
 
-	calendarEl.innerHTML = null;
+  calendarEl.innerHTML = null;
 
-	renderPreviousMonth();
-	renderCurrentMonth();
-	renderNextMonth();
-	calendarEl.innerHTML = null;
-	renderPreviousMonth();
-	renderCurrentMonth();
-	renderNextMonth();
+  renderPreviousMonth();
+  renderCurrentMonth();
+  renderNextMonth();
+  calendarEl.innerHTML = null;
+  renderPreviousMonth();
+  renderCurrentMonth();
+  renderNextMonth();
 }
 
 function renderPreviousMonth() {
-	let firstdayOfMonth = new Date(
-		currentDate.getFullYear(),
-		currentDate.getMonth() + monthOffset,
-		1,
-	);
+  let firstdayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+    1,
+  );
 
-	let daysNeeded = getDayCountForPreviousMonth(firstdayOfMonth.getDay());
+  let daysNeeded = getDayCountForPreviousMonth(firstdayOfMonth.getDay());
 
-	const previousMonth = new Date(
-		currentDate.getFullYear(),
-		currentDate.getMonth() + monthOffset,
-		0 - daysNeeded,
-	);
+  const previousMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+    0 - daysNeeded,
+  );
 
-	for (let i = daysNeeded; i > 0; i--) {
-		let dayDate = new Date(
-			previousMonth.getFullYear(),
-			previousMonth.getMonth(),
-			previousMonth.getDay() - i,
-		);
+  for (let i = daysNeeded; i > 0; i--) {
+    let dayDate = new Date(
+      previousMonth.getFullYear(),
+      previousMonth.getMonth(),
+      previousMonth.getDay() - i,
+    );
 
-		let card = createCalendarCard(dayDate);
-		card.classList.add("shaded-card");
-		calendarEl.append(card);
-	}
+    let card = createCalendarCard(dayDate);
+    card.classList.add("shaded-card");
+    calendarEl.append(card);
+  }
 }
 
 function renderCurrentMonth() {
-	let monthStart = new Date(
-		currentDate.getFullYear(),
-		currentDate.getMonth() + monthOffset,
-		1,
-	);
+  let monthStart = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+    1,
+  );
 
-	let currentMonth = monthStart.getMonth();
+  let currentMonth = monthStart.getMonth();
 
-	while (currentMonth === monthStart.getMonth()) {
-		calendarEl.append(createCalendarCard(monthStart));
+  while (currentMonth === monthStart.getMonth()) {
+    calendarEl.append(createCalendarCard(monthStart));
 
-		monthStart.setDate(monthStart.getDate() + 1);
-	}
+    monthStart.setDate(monthStart.getDate() + 1);
+  }
 }
 
 function renderNextMonth() {
-	let nextMonth = new Date(
-		currentDate.getFullYear(),
-		currentDate.getMonth() + monthOffset + 1,
-		1,
-	);
+  let nextMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset + 1,
+    1,
+  );
 
-	let currentCardCount = calendarEl.childElementCount;
+  let currentCardCount = calendarEl.childElementCount;
 
-	for (let i = currentCardCount; i < calendarDesiredCardCount; i++) {
-		let card = createCalendarCard(nextMonth);
-		card.classList.add("shaded-card");
-		calendarEl.append(card);
-		nextMonth.setDate(nextMonth.getDate() + 1);
-	}
+  for (let i = currentCardCount; i < calendarDesiredCardCount; i++) {
+    let card = createCalendarCard(nextMonth);
+    card.classList.add("shaded-card");
+    calendarEl.append(card);
+    nextMonth.setDate(nextMonth.getDate() + 1);
+  }
 }
 
 function createCalendarCard(date) {
-	let newCard = document.createElement("div");
-	newCard.classList.add("calendar-card");
+  let newCard = document.createElement("div");
+  newCard.classList.add("calendar-card");
 
-	let cardInfo = document.createElement("p");
-	cardInfo.classList.add("calendar-card-info");
-	cardInfo.textContent = date.getDate();
+  let cardInfo = document.createElement("p");
+  cardInfo.classList.add("calendar-card-info");
+  cardInfo.textContent = date.getDate();
 
-	const holiday = getPublicHoliday(new Date(date));
-	if (holiday) {
-		const holidayName = document.createElement("p");
-		holidayName.className = "calendar-holiday-name";
-		holidayName.textContent = holiday;
+  const holiday = getPublicHoliday(new Date(date));
 
-		cardInfo.append(holidayName);
-	}
+  if (holiday) {
+    const holidayText = document.createElement("span");
+    holidayText.classList.add("calendar-holiday-name");
+    holidayText.textContent = holiday;
 
-	// newCard.append(cardInfo);
-	// return newCard;
-	// let newCard = document.createElement("div");
-	// newCard.classList.add("calendar-card");
+    const holidayDot = document.createElement("span");
+    holidayDot.classList.add("calendar-holiday-dot");
+    holidayDot.title = holiday;
 
-	// let cardInfo = document.createElement("p");
-	// cardInfo.classList.add("calendar-card-info");
-	// cardInfo.textContent = date.getDate();
+    cardInfo.append(holidayText);
+    cardInfo.append(holidayDot);
+  }
 
-	newCard.append(cardInfo);
+  // newCard.append(cardInfo);
+  // return newCard;
+  // let newCard = document.createElement("div");
+  // newCard.classList.add("calendar-card");
 
-	let todoCount = countTodosSpecificDate(date);
+  // let cardInfo = document.createElement("p");
+  // cardInfo.classList.add("calendar-card-info");
+  // cardInfo.textContent = date.getDate();
 
-	if (todoCount > 0) {
-		let todoCountBox = document.createElement("div");
-		todoCountBox.classList.add("calendar-dot-box");
+  newCard.append(cardInfo);
 
-		for (let i = 0; i < Math.min(5, todoCount); i++) {
-			let dot = document.createElement("div");
-			dot.classList.add("calendar-dot");
-			todoCountBox.append(dot);
-		};
+  let todoCount = countTodosSpecificDate(date);
 
-		if (todoCount > 5) {
-			let todoCountEl = document.createElement("p");
-			todoCountEl.textContent = `+${todoCount - 5}`;
+  if (todoCount > 0) {
+    let todoCountBox = document.createElement("div");
+    todoCountBox.classList.add("calendar-dot-box");
 
-			todoCountBox.append(todoCountEl)
-		}
+    for (let i = 0; i < Math.min(5, todoCount); i++) {
+      let dot = document.createElement("div");
+      dot.classList.add("calendar-dot");
+      todoCountBox.append(dot);
+    }
 
-		newCard.append(todoCountBox);
-	}
+    if (todoCount > 5) {
+      let todoCountEl = document.createElement("p");
+      todoCountEl.textContent = `+${todoCount - 5}`;
+      todoCountBox.append(todoCountEl);
+    }
 
-	return newCard;
+    newCard.append(todoCountBox);
+  }
+
+  return newCard;
 }
 
 function countTodosSpecificDate(date) {
-	const todosOnDate = todos.filter((t) =>
-		convertToLocaleDateString(t.startAt) == convertToLocaleDateString(date))
+  const todosOnDate = todos.filter(
+    (t) =>
+      convertToLocaleDateString(t.startAt) == convertToLocaleDateString(date),
+  );
 
-	return todosOnDate.length;
+  return todosOnDate.length;
 }
 
 function convertToLocaleDateString(date) {
-	return date.toLocaleDateString("sv-SE", dateOptions);
+  return date.toLocaleDateString("sv-SE", dateOptions);
 }
 
 function setMonthYearInfo() {
-	monthYearInfoEl.textContent = null;
-	let date = new Date(
-		currentDate.getFullYear(),
-		currentDate.getMonth() + monthOffset,
-	);
+  monthYearInfoEl.textContent = null;
+  let date = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + monthOffset,
+  );
 
-	monthYearInfoEl.textContent = date.toLocaleDateString("sv", {
-		month: "long",
-		year: "numeric",
-	});
+  monthYearInfoEl.textContent = date.toLocaleDateString("sv", {
+    month: "long",
+    year: "numeric",
+  });
 }
 
 function changeMonth(change) {
-	monthOffset += change;
+  monthOffset += change;
 
-	renderCalendar();
+  renderCalendar();
 }
 
 function getDayCountForPreviousMonth(dayOfWeek) {
-	switch (dayOfWeek) {
-		case 1:
-			return 0;
-		case 2:
-			return 1;
-		case 3:
-			return 2;
-		case 4:
-			return 3;
-		case 5:
-			return 4;
-		case 6:
-			return 5;
-		case 0:
-			return 6;
-		default:
-			return -1;
-	}
+  switch (dayOfWeek) {
+    case 1:
+      return 0;
+    case 2:
+      return 1;
+    case 3:
+      return 2;
+    case 4:
+      return 3;
+    case 5:
+      return 4;
+    case 6:
+      return 5;
+    case 0:
+      return 6;
+    default:
+      return -1;
+  }
 }
 
 let holidays = [];
 
 async function initHolidays() {
-	const res = await fetch("https://api.dagsmart.se/holidays");
+  const res = await fetch("https://api.dagsmart.se/holidays");
 
-	if (!res.ok) {
-		console.error("Error", res.status);
-		holidays = [];
-		return;
-	}
-	holidays = await res.json();
+  if (!res.ok) {
+    console.error("Error", res.status);
+    holidays = [];
+    return;
+  }
+  holidays = await res.json();
 }
 
 function getPublicHoliday(date) {
-	const dateStr = date.toLocaleDateString("sv-SE");
+  const dateStr = date.toLocaleDateString("sv-SE");
 
-	let match;
+  let match;
 
-	for (let index = 0; index < holidays.length; index++) {
-		if (holidays[index].date === dateStr) {
-			match = toCapitalCase(holidays[index].name.sv);
-		}
-	}
+  for (let index = 0; index < holidays.length; index++) {
+    if (holidays[index].date === dateStr) {
+      match = toCapitalCase(holidays[index].name.sv);
+    }
+  }
 
-	if (!match) {
-		return null;
-	}
-	return toCapitalCase(match);
+  if (!match) {
+    return null;
+  }
+  return toCapitalCase(match);
 }
 
 function toCapitalCase(str) {
-	let holiday = "";
+  let holiday = "";
 
-	if (str.includes(" ")) {
-		const firstWord = str.split(" ")[0];
-		const secondWord = str.split(" ")[1];
+  if (str.includes(" ")) {
+    const firstWord = str.split(" ")[0];
+    const secondWord = str.split(" ")[1];
 
-		const firstWordCapitalized =
-			firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
-		const secondWordCapitalized =
-			secondWord.charAt(0).toUpperCase() + secondWord.slice(1);
-		holiday = `${firstWordCapitalized} ${secondWordCapitalized}`;
-	} else {
-		holiday = str.charAt(0).toUpperCase() + str.slice(1);
-	}
+    const firstWordCapitalized =
+      firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
+    const secondWordCapitalized =
+      secondWord.charAt(0).toUpperCase() + secondWord.slice(1);
+    holiday = `${firstWordCapitalized} ${secondWordCapitalized}`;
+  } else {
+    holiday = str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
-	return holiday;
+  return holiday;
 }
